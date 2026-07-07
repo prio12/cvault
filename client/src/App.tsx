0b2b76ef593d1798500ec35512e5823a491e41de
prio12 <maksudurprio.mp@gmail.com>
2026-07-07 18:32:47 +0600

    feat: initial setup done

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="aura">
+        <div className="card bg-base-100">
+          <div className="card-body">
+            <p>This card has aura</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
